67bb6a02abe498bcc2ac64fa9ae9b48ab924c95b
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-24 19:07:25 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -74,37 +74,62 @@ function sendFBMessage(sender, messageData) {
     
     var APISpeech = messageData.text;
     if(APISpeech == "structured"){
-        messageData = {
-            "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "generic",
-                "elements": [{
-                  "title": "First card",
-                  "subtitle": "Element #1 of an hscroll",
-                  "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                  "buttons": [{
-                    "type": "web_url",
-                    "url": "https://www.messenger.com/",
-                    "title": "Web url"
-                  }, {
-                    "type": "postback",
-                    "title": "Postback",
-                    "payload": "Payload for first element in a generic bubble",
-                  }],
-                },{
-                  "title": "Second card",
-                  "subtitle": "Element #2 of an hscroll",
-                  "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                  "buttons": [{
-                    "type": "postback",
-                    "title": "Postback",
-                    "payload": "Payload for second element in a generic bubble",
-                  }],
-                }]
-              }
-            }
-          };
+        messageData = "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"receipt",
+        "recipient_name":"Stephane Crozatier",
+        "order_number":"12345678902",
+        "currency":"USD",
+        "payment_method":"Visa 2345",        
+        "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+        "timestamp":"1428444852", 
+        "elements":[
+          {
+            "title":"Classic White T-Shirt",
+            "subtitle":"100% Soft and Luxurious Cotton",
+            "quantity":2,
+            "price":50,
+            "currency":"USD",
+            "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+          },
+          {
+            "title":"Classic Gray T-Shirt",
+            "subtitle":"100% Soft and Luxurious Cotton",
+            "quantity":1,
+            "price":25,
+            "currency":"USD",
+            "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+          }
+        ],
+        "address":{
+          "street_1":"1 Hacker Way",
+          "street_2":"",
+          "city":"Menlo Park",
+          "postal_code":"94025",
+          "state":"CA",
+          "country":"US"
+        },
+        "summary":{
+          "subtotal":75.00,
+          "shipping_cost":4.95,
+          "total_tax":6.19,
+          "total_cost":56.14
+        },
+        "adjustments":[
+          {
+            "name":"New Customer Discount",
+            "amount":20
+          },
+          {
+            "name":"$10 Off Coupon",
+            "amount":10
+          }
+        ]
+      }
+    }
+  };
     }
     
     request({
