4fa0675bdb60c7eb1bc80247e3209f9e12e07107
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-24 16:55:09 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -69,13 +69,44 @@ function processEvent(event) {
 }
 
 function sendFBMessage(sender, messageData) {
+    var messageStructured = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: FB_PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id: sender},
-            message: messageData 
+            message: messageStructured 
         }
     }, function (error, response, body) {
         if (error) {
