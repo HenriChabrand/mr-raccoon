f873d7ee3074170b83896dde8cf83630461824c4
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-25 14:35:17 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ function processEvent(event) {
 
 function sendFBMessage(sender, messageData) {
     
-    var APISpeech = require('./powerlevel')(9050);
+    var APISpeech = require('powerlevel')(9050);
     if(APISpeech == "structured"){
         messageData = {
             "attachment": {
