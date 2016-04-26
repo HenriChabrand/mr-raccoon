e917a700324bf4c8a562ccdba2e8dc3073136b66
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 12:46:15 +0200

    Create RATP_App.js

---
+++ src/RATP_App.js
@@ -0,0 +1,24 @@
+
+var num_ligne = 3;
+
+var info = function(num_ligne) {
+  
+  var result_value = {status :"ini"};
+  var request = require('request');
+  request('http://api-ratp.pierre-grimaud.fr/v2/traffic/metros/'+num_ligne, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body)
+      
+        result_value = info.response.message;
+    }else{
+        
+        result_value = {status :"erro"};
+    }
+  })
+
+    return  result_value;
+};
+
+
+module.exports.num_ligne = num_ligne;
+module.exports.info = info;
