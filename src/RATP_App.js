8557af88220c6eb17d2c64b6fb9a3dd6437afcac
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 15:17:18 +0200

    Update RATP_App.js

--- src/RATP_App.js
+++ src/RATP_App.js
@@ -3,39 +3,6 @@ var num_ligne = 3;
 
 var info = function(num_ligne) {
   
-  (function (exports) {
-  'use strict';
- 
-  var Sequence = exports.Sequence || require('sequence').Sequence
-    , sequence = Sequence.create()
-    , err
-    ;
- 
-  sequence
-    .then(function (next) {
-      setTimeout(function () {
-        next(err, "Hi", "World!");
-      }, 120);
-      console.log("Text", "then 1");
-    })
-    .then(function (next, err, a, b) {
-      setTimeout(function () {
-        next(err, "Hello", b);
-      }, 270);
-      console.log("Text", "then 2");
-    })
-    .then(function (next, err, a, b) {
-      setTimeout(function () {
-        console.log(a, b);
-        next();
-      }, 50);
-       console.log("Text", "then 3");
-    });
- 
-// so that this example works in browser and node.js 
-}('undefined' !== typeof exports && exports || new Function('return this')()));
-  
-  
   return { end : "end"};
   };
 
