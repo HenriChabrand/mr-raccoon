d2878dc224acbeb2cd57a571a11a22c2a77ea61c
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 19:23:06 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -72,9 +72,12 @@ function processEvent(event) {
                     */
                     
                     var action_module = require('./RATP_App.js');
-                    var result_value = action_module.info();
-                   
-                        sendFBMessage(sender, result_value);
+                   action_module.info(function(result) {
+                         var returedData = result
+                    
+                         sendFBMessage(sender, returedData);
+                    }));
+                       
                    
                     
                 }
