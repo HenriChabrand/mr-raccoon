4cb5dd5de31c881e8e5464f85f1e669d09131b2a
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 20:14:31 +0200

    Update RATP_App.js

--- src/RATP_App.js
+++ src/RATP_App.js
@@ -2,27 +2,17 @@
 
 function getResult(callback, parameters) {
   
-  var result_value = {status : "ini"};
-  console.log("res 0 ",result_value);
-  
-  var request = require('request');
-  
   var num_line = parameters.num_line;
   
-  
-  request('http://api-ratp.pierre-grimaud.fr/v2/traffic/metros/'+num_line, function (error, response, body) {
+  require('request')('http://api-ratp.pierre-grimaud.fr/v2/traffic/metros/'+num_line, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
-          result_value = {result : info.response.message};
-          callback(result_value.result + num_line); 
-
+        callback(info.response.message + num_line); 
       }else{
-          result_value = {status :"erro"};
-          callback(result_value);
+          callback({status :"erro"});
       }
-      
-  
     });
+    
   };
 
 exports.getResult = getResult;
