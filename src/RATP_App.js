af29450476fbbf5deeb626fa4d84b8735261d6cf
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 19:27:11 +0200

    Update RATP_App.js

--- src/RATP_App.js
+++ src/RATP_App.js
@@ -9,21 +9,15 @@ function info(callback) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body)
           result_value = {result : info.response.message};
-          response.on('data', function (chunk) {
-                result_value += chunk;
-                callback(result_value);
-           }); 
+          callback(result_value); 
 
       }else{
           result_value = {status :"erro"};
+          callback(result_value);
       }
       
   
     });
-    
-  console.log("res 2 ",result_value);
-  
-  return result_value;
   };
 
 exports.info = info;
