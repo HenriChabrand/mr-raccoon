9173087fb4f975898ceae9ac9caa7acb6922d943
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 12:54:54 +0200

    Update RATP_App.js

--- src/RATP_App.js
+++ src/RATP_App.js
@@ -5,13 +5,16 @@ var info = function(num_ligne) {
   
   var result_value = {status :"ini"};
   
+  console.log("RATP : ", 'Ini Global');
   request('http://api-ratp.pierre-grimaud.fr/v2/traffic/metros/13', function (error, response, body) {
+    console.log("RATP : ", 'Ini request');
     if (!error && response.statusCode == 200) {
+      console.log("RATP : ", 'Ini if');
       var info = JSON.parse(body)
       
         result_value = info.response.message;
     }else{
-        
+        console.log("RATP : ", 'Ini else');
         result_value = {status :"erro"};
     }
   });
