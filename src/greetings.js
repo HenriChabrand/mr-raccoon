163aa337272e1168ea2e1fe95f578ce5e77f38f6
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-26 11:12:35 +0200

    Update greetings.js

--- src/greetings.js
+++ src/greetings.js
@@ -1,21 +1,7 @@
 var x = 'Parameters : ';
 var addX = function(value) {
   
-  request({
-        url: 'http://api-ratp.pierre-grimaud.fr/v2/metros/8/stations/275',
-        method: 'GET',
-        json: {
-            destination : 23
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-        
-        console.log(SON.stringify(response));
-    });
+  
     
     return  x + JSON.stringify(value) +" to prosses";
 };
