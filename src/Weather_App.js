1b21c4e56ef61db78d57132c7d16bd4d7e107c43
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-27 15:43:56 +0200

    Update Weather_App.js

--- src/Weather_App.js
+++ src/Weather_App.js
@@ -7,7 +7,7 @@ function getResult(callback, parameters) {
         require('request')('https://api.forecast.io/forecast/5fe274f52b8b66a83b716c68ff4da61f/'+parameters.lat+','+parameters.lng, function (error, response, body) {
             if (!error && response.statusCode == 200) {
               var info = JSON.parse(body);
-              callback(JSON.stringify(info)); 
+              callback(JSON.stringify(info.currently.summary)); 
             }else{
                 callback({erro :"the request failed"});
             }
