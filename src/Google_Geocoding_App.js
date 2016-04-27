668de67aab4dacdafd4fee4944b0a6e49c6b52f8
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-27 15:15:19 +0200

    Update Google_Geocoding_App.js

--- src/Google_Geocoding_App.js
+++ src/Google_Geocoding_App.js
@@ -3,8 +3,8 @@ function getResult(callback, parameters) {
     if(parameters.geo_city){
         //Put your action here
         var city = parameters.geo_city;
-        
-        require('request')('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDLGejsdWAYT5BKgqqvmRQUoWtT34OWfE8&address='+city, function (error, response, body) {
+        var country = 'FR';
+        require('request')('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDLGejsdWAYT5BKgqqvmRQUoWtT34OWfE8&address='+city+','+country, function (error, response, body) {
             if (!error && response.statusCode == 200) {
               var info = JSON.parse(body);
               callback(JSON.stringify(info.results[1].geometry.location)); 
