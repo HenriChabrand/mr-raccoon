287b716336989271aaf5696e2817a82a8501e054
HenriChabrand <GlobalAIConector@gmail.com>
2016-04-28 15:00:05 +0200

    Update Google_Geocoding_App.js

--- src/Google_Geocoding_App.js
+++ src/Google_Geocoding_App.js
@@ -7,15 +7,15 @@ function getResult(callback, parameters) {
         require('request')('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDLGejsdWAYT5BKgqqvmRQUoWtT34OWfE8&address='+city+','+country, function (error, response, body) {
             if (!error && response.statusCode == 200) {
               var info = JSON.parse(body);
-              callback(JSON.stringify(info.results[0].geometry.location)); 
+              callback(info.results[0].geometry.location); 
             }else{
-                callback({erro :"the request failed"});
+                callback("the request failed");
             }
           });
           
     }else{
             
-        callback({erro :"parameters are missing"});
+        callback("parameters are missing");
     }
     
 }
