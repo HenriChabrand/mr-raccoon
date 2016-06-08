ac17271efffc01417af2ec18a9e3cc848dbe8f64
HenriChabrand <GlobalAIConector@gmail.com>
2016-06-08 16:11:37 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const uuid = require('node-uuid');
 const request = require('request');
 
 const REST_PORT = (process.env.PORT || 5000);
-const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_ACCESS_TOKEN = '798d7a884de94566872e37c9959f1712';
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
