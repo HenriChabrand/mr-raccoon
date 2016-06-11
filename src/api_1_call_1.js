cb60ac610bc85e67fe7d49a44dd544c8e60ad61f
HenriChabrand <GlobalAIConector@gmail.com>
2016-06-11 20:49:40 +0200

    Update api_1_call_1.js

--- src/api_1_call_1.js
+++ src/api_1_call_1.js
@@ -2,7 +2,7 @@ function getResult(callback, parameters) {
 
     if(parameters.city){
         //Put your action here
-        var city = parameters.geo_city;
+        var city = parameters.city;
         var country = 'FR';
         require('request')('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDLGejsdWAYT5BKgqqvmRQUoWtT34OWfE8&address='+city+','+country, function (error, response, body) {
             if (!error && response.statusCode == 200) {
