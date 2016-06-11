ce90356949f0b7f1b8f8d161a921b6acd36d12a9
HenriChabrand <GlobalAIConector@gmail.com>
2016-06-11 23:28:58 +0200

    Update api_2_call_2.js

--- src/api_2_call_2.js
+++ src/api_2_call_2.js
@@ -33,7 +33,7 @@ function getResult(callback, json) {
         
         json.index = json.index+1;
         if(json.step[json.index].nb=="end"){
-            callback(json);
+            callback(json.input);
         }else{
              var action_module = require('./'+ json.step[json.index].call_id + '.js');
              action_module.getResult(function(result) {
