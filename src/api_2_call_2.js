c4a0f7d587facff3704396690c27e7092b039bc5
HenriChabrand <GlobalAIConector@gmail.com>
2016-06-11 23:09:36 +0200

    Update api_2_call_2.js

--- src/api_2_call_2.js
+++ src/api_2_call_2.js
@@ -14,7 +14,8 @@ function getResult(callback, json) {
     if(all_required){
         
         var date_ISO_8601 = date + 'T';
-        
+         console.log("input api_2_call_2 : ", input);
+        /*
         if(input.time){
             console.log("input api_2_call_2 : ", input);
             console.log("time api_2_call_2 : ");
@@ -23,7 +24,7 @@ function getResult(callback, json) {
         }else{
             date_ISO_8601 += '00:00:00';
         }
-        
+        */
         date_ISO_8601 += 'Z';
         
         
