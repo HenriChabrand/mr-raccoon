2a1a2c0428d4bff7a8807478b7a088843d057f0d
HenriChabrand <GlobalAIConector@gmail.com>
2016-06-11 22:24:22 +0200

    Update api_2_call_2.js

--- src/api_2_call_2.js
+++ src/api_2_call_2.js
@@ -2,22 +2,21 @@ function getResult(callback, json) {
     
     console.log("Call made : api_2_call_2");
      
+   
     var res = 'yolo';
+    
+    
     
     json.index = json.index+1;
     if(json.step[json.index].nb=="end"){
-         callback(res); 
-     }else{
+        callback(json);
+    }else{
          var action_module = require('./'+ json.step[json.index].call_id + '.js');
          action_module.getResult(function(result) {
-             console.log(json.step[json.index].call_id + " : " + result);
-             callback(result); 
-             
+            callback(result); 
          },json);
-     }   
+     }  
      
-     
-     callback(json); 
 }
 
 exports.getResult = getResult;
