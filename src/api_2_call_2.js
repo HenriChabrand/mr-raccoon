fe6218f1c4863b217ac7cb9d28e7e4e6624b0c34
HenriChabrand <GlobalAIConector@gmail.com>
2016-06-11 23:01:35 +0200

    Update api_2_call_2.js

--- src/api_2_call_2.js
+++ src/api_2_call_2.js
@@ -2,20 +2,42 @@ function getResult(callback, json) {
     
     console.log("Call made : api_2_call_2");
      
-   
-    json.input.temperature = 23;
-    json.input.humidity = 0.67;
+    var input = json.input;
     
-    json.index = json.index+1;
-    if(json.step[json.index].nb=="end"){
-        callback(json);
-    }else{
-         var action_module = require('./'+ json.step[json.index].call_id + '.js');
-         action_module.getResult(function(result) {
-            callback(result); 
-         },json);
-     }  
-     
+    var all_required = false;
+    if(input.geoCode&&input.date){
+        var date = input.date;
+        var geoCode = input.geoCode;
+        all_required = true;
+    }
+    
+    if(all_required){
+        
+        var date_ISO_8601 = date + 'T';
+        
+        if(input.time){
+            var time = input.time;
+            date_ISO_8601 += time;
+        }else{
+            date_ISO_8601 += '00:00:00';
+        }
+        
+        date_ISO_8601 += 'Z';
+        
+        
+        
+        json.input.date_ISO_8601 = date_ISO_8601;
+        
+        json.index = json.index+1;
+        if(json.step[json.index].nb=="end"){
+            callback(json);
+        }else{
+             var action_module = require('./'+ json.step[json.index].call_id + '.js');
+             action_module.getResult(function(result) {
+                callback(result); 
+             },json);
+        }  
+    } 
 }
 
 exports.getResult = getResult;
